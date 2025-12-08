bb1247c9f038f425e96e5b1a3336a36f0bb3fb8f
47work <sinkvadev@gmail.com.>
2025-12-08 17:12:46 +0700

    added navLink hover effect + fixed ThemeBtn styles

--- src/components/ui/navLink/NavLink.jsx
+++ src/components/ui/navLink/NavLink.jsx
@@ -8,6 +8,9 @@ export function NavLink({ activeTab, path, name, linkKey }) {
       className={`${styles.navigationLink} ${activeTab === linkKey ? styles.navigationLinkActive : ''}`}
       href="#"
     >
+      <svg className={styles.navOutline} xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+        <rect className={styles.navRect} x="0" y="0" rx="20" ry="20" width="100%" height="100%" />
+      </svg>
       {name}
     </Link>
   )
--- src/pages/home/App.jsx
+++ src/pages/home/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <section className={styles.banner}>
         <div className={styles.bannerInfo}>
           <h1 className={styles.title}>From Concept to Live Server</h1>
-          <h2 className={styles.text}>Full-cycle web development for founders. I transform ideas into production-ready, scalable websites and tools, handling every step from UI/UX to deployment.</h2>
+          <h2 className={styles.text}>Full-cycle web development for founders. <br /> I transform ideas into production-ready, scalable websites and tools, handling every step from UI/UX to deployment.</h2>
         </div>
         <div className={styles.bannerImage}>
         </div>

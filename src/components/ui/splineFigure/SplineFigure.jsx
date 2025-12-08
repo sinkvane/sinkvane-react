5e45e122819e97f0cccbf24a03949dc2f6b3749b
sinkvane <sinkvadev@gmail.com>
2025-12-08 22:46:31 +0700

    spline started

---
+++ src/components/ui/splineFigure/SplineFigure.jsx
@@ -0,0 +1,23 @@
+import Spline from "@splinetool/react-spline";
+import { useState } from "react"
+import styles from './splineFigure.module.scss';
+
+export function SplineFigure() {
+  const [loaded, setLoaded] = useState(false);
+
+  return (
+    <div className={styles.splineContainer}>
+
+      {!loaded && (
+        <div style={{ textAlign: 'center' }}>
+          loading..
+        </div>
+      )}
+
+      <Spline
+        scene='/main_screen.spline'
+        onLoad={() => setLoaded(true)}
+      />
+    </div >
+  )
+}
--- src/pages/home/App.jsx
+++ src/pages/home/App.jsx
@@ -1,16 +1,20 @@
+import { Button } from '@/components/ui/button/Button';
 import styles from '@/pages/home/app.module.scss';
+import { SplineFigure } from '@/components/ui/splineFigure/SplineFigure';
 
 function App() {
-  
+
   return (
     <>
-      <section className={styles.banner}>
-        <div className={styles.bannerInfo}>
+      <section className={styles.main}>
+        <div className={styles.mainInfo}>
           <h1 className={styles.title}>From Concept to Live Server</h1>
           <h2 className={styles.text}>Full-cycle web development for founders. <br /> I transform ideas into production-ready, scalable websites and tools, handling every step from UI/UX to deployment.</h2>
+          <Button text={'see more'} />
         </div>
-        <div className={styles.bannerImage}>
-        </div>
+        {/* <div className={styles.mainImage}>
+          <SplineFigure />
+        </div> */}
       </section>
     </>
   )

cb1b3b65b4cfb9515d83f5523499810c4aea5159
sinkvane <sinkvadev@gmail.com>
2025-12-21 22:47:26 +0700

    gsap package added & smooth scroll added to main layout

--- src/pages/home/App.jsx
+++ src/pages/home/App.jsx
@@ -13,10 +13,7 @@ function App() {
         <div className={styles.mainInfo}>
           <h1 className={styles.title}>{t('main.title')}</h1>
           <h2 className={styles.text}>{t('main.description')}.</h2>
-          <div className={styles.mainButtons}>
-            <Button text={t('buttons.seemore')} />
-            <Button anchor={'./works'} text={t('buttons.works')} />
-          </div>
+          <Button text={t('buttons.seemore')} />
         </div>
       </section>
 
